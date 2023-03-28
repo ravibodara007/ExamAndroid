68a6ae6ba5106793befc3db28444394b45445cd8
BoyWithNoHeart007 <ravibodara73@gmail.com>
2023-03-28 12:01:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity();
+            }
+        });
+    }
+}
